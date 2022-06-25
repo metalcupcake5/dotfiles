c8ff2fd3b8bdb0e50a0ef84de50bc2565364c65e
metalcupcake5 <imcoolcom5@gmail.com>
2022-06-24 17:30:06 -0700

    remove eww

--- eww/launch.sh
+++
@@ -1,9 +0,0 @@
-~/projects/eww/target/release/eww close-all
-if [[ "$1" == "stop" ]]; then
-    echo "stopped"
-else
-    echo "starting"
-    ~/projects/eww/target/release/eww open background
-    ~/projects/eww/target/release/eww open menu
-    echo "started"
-fi
